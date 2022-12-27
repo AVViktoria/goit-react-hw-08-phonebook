d324837068cba819bcc9afc3e6f1c1fe7d31924a
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-12-26 20:01:36 -0800

    add Navigation, AuthNav, UserMenu, AppBar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,75 +1,64 @@
-// import { Route, Routes } from 'react-router-dom';
-// import { useEffect, Suspense, lazy } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 
 //*      Components      //
-import ContactList from 'components/ContactList';
-import ContactForm from 'components/ContactForm';
-import Filter from 'components/Filter';
-import Container from 'components/Container';
-import Section from 'components/Section/Section';
+// import ContactList from 'components/ContactList';
+// import ContactForm from 'components/ContactForm';
+// import Filter from 'components/Filter';
+// import Container from 'components/Container';
+// import Section from 'components/Section/Section';
 
 //*      styles      //
 import '../index.scss';
 
-// import { useEffect, Suspense, lazy } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-
 //*      Auth selectors      //
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import {useEffect, Suspense, lazy } from 'react';
+// import { Switch } from 'react-router-dom';
 import {authOperations, authSelectors} from '../redux/auth'
 
-  // import { useEffect, Suspense, lazy } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { Switch } from 'react-router-dom';
-// import AppBar from './components/AppBar';
-// import Container from './components/Container';
-// import PrivateRoute from './components/PrivateRoute';
-// import PublicRoute from './components/PublicRoute';
-// import { authOperations, authSelectors } from './redux/auth';
+//*      Lazy pages      //
 
-// const HomeView = lazy(() => import('./views/HomeView'));
-// const RegisterView = lazy(() => import('./views/RegisterView'));
-// const LoginView = lazy(() => import('./views/LoginView'));
-// const TodosView = lazy(() => import('./views/TodosView'));
-// const UploadView = lazy(() => import('./views/UploadView'));
+const LoginView = lazy(() => import('../views/LoginView'));
+const RegisterView = lazy(() => import('../views/RegisterView'));
+const HomeView = lazy(() => import('../views/HomeView'));
+const AppBar = lazy(() => import('../components/AppBar'));
+const Phonebook = lazy(() => import('../components/Phonebook'));
+const UploadView = lazy(() => import('../views/UploadView'));
 
 //*      Root      //
 export default function App() {
   const dispatch = useDispatch();
-  const isFetchingCurrentUser = useSelector(authSelectors.getIsLoggedIn);
-  console.log(isFetchingCurrentUser);
-//  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
+  // const isFetchingCurrentUser = useSelector(authSelectors.getIsLoggedIn);
+  // console.log(isFetchingCurrentUser);
+   const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
   
 
   useEffect(() => {
-    // dispatch(authOperations.fetchCurrentUser());
-    dispatch(authOperations({
-       "email": "test@teaty.com",
-  "password": "pas12345"
-    }))
+    dispatch(authOperations.fetchCurrentUser());
+    // dispatch(authOperations({
+    //   "email": "test@teaty.com",
+    //   "password": "pas12345"
+    // }))
   }, [dispatch]);
-  return (
-    <Container>
-      {/* {isFetchingCurrentUser ? (
-        <h1>Показываем React Skeleton</h1>
-      ) : ( */}
-        <>
-          <Section>
-            <Container>
-              <h1 className="title">Phonebook</h1>
-              <ContactForm />
-            </Container>
-            <Container>
-              <h2 className="title">Contacts</h2>
-              <Filter />
-              <ContactList
-              />
-            </Container>
-          </Section>
-      </>
-      {/* )} */}
-    </Container>
+  return(
+  isFetchingCurrentUser ? (
+    <h1>Показываем React Skeleton</h1>
+  ) : (
+    <div>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+          <Route path="/" element={<AppBar />}/> 
+          <Route index element={<HomeView />} />
+          <Route path="/login" element={<LoginView />} redirectTo={'./contacts'} />
+          <Route path="/register" element={<RegisterView />} redirectTo={'./contacts'} />
+          <Route path="/contacts" element={<Phonebook />} redirectTo={'./login'} />
+          <Route path="/upload" element={<UploadView />}  redirectTo="/login" />
+          <Route path="*" element={<LoginView />} />
+         </Routes>
+         </Suspense> 
+      
+    </div>
+  )
   );
-}
+};
---
+++ src/components/Phonebook/Phonebook.js
@@ -0,0 +1,12 @@
+//  <Section>
+//           <Container>
+//             <h1 className="title">Phonebook</h1>
+//             <ContactForm />
+//           </Container>
+//           <Container>
+//             <h2 className="title">Contacts</h2>
+//             <Filter />
+//             <ContactList
+//             />
+//           </Container>
+//         </Section>
